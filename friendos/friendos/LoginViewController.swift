d4ebb73e99e1fae700d64e958b3ee1086f412e7c
CheshireCat16 <john.cheshire@gmail.com>
2022-03-16 18:48:14 -0230

    Added Login screen and login view controller

---
+++ friendos/friendos/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  friendos
+//
+//  Created by John Cheshire on 3/16/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

1fee5b02905618620d4aaddbc58110de4340b435
Daniel Ruiz <ruizdan@oregonstate.edu>
2022-03-15 19:22:17 -0700

    Initial commit.

---
+++ friendos/friendos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  friendos
+//
+//  Created by Daniel Ruiz on 3/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

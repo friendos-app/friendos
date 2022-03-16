ed7a566e9947a53ae0eea912e246aee10a8786b6
CheshireCat16 <john.cheshire@gmail.com>
2022-03-16 18:58:57 -0230

    Added login function and view controller for Home screen

--- friendos/friendos/LoginViewController.swift
+++ friendos/friendos/LoginViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    // Variables that hold username and password
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    // Runs upon view loading
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    // Sign up a new user and segue to the profile screen
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    // Login a user and segue to the Home screen
+    @IBAction func onLogin(_ sender: Any) {
+        // Login with the specified username and password
+        PFUser.logInWithUsername(inBackground: self.userNameText.text!, password: self.passwordText.text!) {
+          (user: PFUser?, error: Error?) -> Void in
+          // If we have a user, we can login and perform the segue to home
+          if user != nil {
+            print("login success")
+              self.performSegue(withIdentifier: "goLoginHome", sender: nil)
+          } else {
+            print(error!.localizedDescription)
+          }
+        }
+    }
 }

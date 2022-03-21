fda6bf19357fe0665ff6cae4e0ee1485e9bfbace
Daniel Ruiz <ruizdan@oregonstate.edu>
2022-03-21 15:15:53 -0700

    Add editProfile
    Added editProfile view and cancel button.

---
+++ friendos/friendos/EditProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EditProfileViewController.swift
+//  friendos
+//
+//  Created by Daniel Ruiz on 3/21/22.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func submitEdit(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

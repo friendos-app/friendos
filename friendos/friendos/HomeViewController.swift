f3565ff29f8bc6a538075e94e0901caeb1dbd1df
Daniel Ruiz <ruizdan@oregonstate.edu>
2022-03-22 20:25:00 -0700

    Work on Main Tab in Group
    We worked on main tab in a group during class time.

--- friendos/friendos/HomeViewController.swift
+++ friendos/friendos/HomeViewController.swift
@@ -7,11 +7,71 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import Parse
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    @IBOutlet weak var UserProfiles: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UserProfiles.dequeueReusableCell(withIdentifier: "UserCell") as! UserCell
+        cell.UserUsername!.text = "Dan"
+        cell.UserBio!.text = "Hey"
+        
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        let user = PFUser.current()
+        // Get user bio and photo
+        let bio = user?["bio"]
+        let photo = user?["image"]
 
+        // Find interests
+        let query = PFQuery(className: "UserInterests")
+        query.includeKey("interest_id")
+        query.whereKey("user_id", equalTo: user)
+
+        // String to hold interests
+        var interests = [String]()
+
+        // Loop through results and put interests into the array
+        // This happens in the background, so need call reload view when complete
+        query.findObjectsInBackground(block: { (objects, error) in
+            if (error == nil) {
+                if let interest_list = objects{
+                    for interest in interest_list {
+                        let cur_interest = interest["interest_id"] as! PFObject
+                        interests.append(cur_interest["interest"] as! String)
+                    }
+
+                    print(interests)
+                }
+
+            } else {
+                print("Error")
+            }
+        })
+        
+        
+
+        UserProfiles.dataSource = self
+        UserProfiles.delegate = self
+        
+        
         // Do any additional setup after loading the view.
     }
     
---
+++ friendos/friendos/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  friendos
+//
+//  Created by Daniel Ruiz on 3/22/22.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    
+    
+    @IBOutlet weak var UserImage: UIImageView!
+    
+    @IBOutlet weak var UserUsername: UILabel!
+    
+    @IBOutlet weak var UserBio: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
